3c4ff9b6244a3cb907e0a474b2abaa8b77319aae
rohithsrinivas <rohithsrinivas.94@gmail.com>
2018-04-22 10:24:56 +0530

    adding config for postgre sql

--- src/main/java/com/restaurant/config/RestaurantConfig.java
+++ src/main/java/com/restaurant/config/RestaurantConfig.java
@@ -1,8 +1,14 @@
 package com.restaurant.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -19,4 +25,13 @@ public class RestaurantConfig {
 	public RestaurantService restaurantService() {
 		return new RestaurantServiceImpl();
 	}
+	
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
 }
